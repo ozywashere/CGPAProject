59cbdffd3f6aac760ee8b9bc6593ddf34725a7d6
ozywashere <durduarslantas@ogr.iu.edu.tr>
2023-08-14 23:24:22 +0300

    CGPA project completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    //
+    //CGPA
+        Scanner input=new Scanner(System.in);
+        int math,physic,english,chemistry,music,history;
+        System.out.println("Math Grade: " );
+        math=input.nextInt();
+        System.out.println("Physic  Grade: ");
+        physic=input.nextInt();
+        System.out.println("English Grade : ");
+        english=input.nextInt();
+        System.out.println("Chemistry Grade: ");
+        chemistry=input.nextInt();
+        System.out.println("Music Grade : ");
+        music=input.nextInt();
+        System.out.println("History Grade");
+        history=input.nextInt();
+        double result=(math + physic + english + chemistry + music + history) / 5;
+        System.out.println("Your Result : " + result);
     }
 }
